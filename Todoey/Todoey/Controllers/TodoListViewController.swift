8fb16391462b74286eabfb21dd6cd6ef4cf6e132
Code Stuff <beau@codestuff.us>
2019-04-05 18:20:45 -0700

    Core Data:  - SQLite data creates  - SQLite data does not load yet, only creates.

--- Todoey/Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Todoey/Controllers/TodoListViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
     
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+//    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    // context for sabing core data
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     // data saved to pList file
 //    let defaults = UserDefaults.standard
@@ -19,11 +22,11 @@ class TodoListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        print("dataFilePath:\t\(dataFilePath)")
+        // where is data being stoed?
+        print("directory for .documentDirectory, in .userDomainMask: \n\(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))")
         
         // load the item pList data
-        loadItems()
+//        loadItems()
         
     }
     
@@ -63,11 +66,12 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen once the user clicks the "add item" button on the alert
-            let newItem = Item()
+            
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
             self.itemArray.append(newItem)
             // save to default local data
-//            self.defaults.setValue(self.itemArray, forKey: "ToDoListArrayItem")
             // make encoder
             self.saveItems()
             
@@ -85,26 +89,24 @@ class TodoListViewController: UITableViewController {
     
     // MARK: model manipulation methods
     func saveItems(){
-        let encoder = PropertyListEncoder()
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+            try context.save()
         } catch {
-            print("error encoding itemArray:\n\(error)")
+            print("error saving context:\n\(error)")
         }
     }
     
     // load data from pList, and decode the data
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-                itemArray = try decoder.decode([Item].self, from: data)
-            } catch {
-                print("Error decoding: \(error)")
-            }
-        }
-    }
+//    func loadItems() {
+//        if let data = try? Data(contentsOf: dataFilePath!) {
+//            let decoder = PropertyListDecoder()
+//            do {
+//                itemArray = try decoder.decode([Item].self, from: data)
+//            } catch {
+//                print("Error decoding: \(error)")
+//            }
+//        }
+//    }
     
 
 
