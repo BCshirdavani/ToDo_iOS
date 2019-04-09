e8dc34e3f4e54e015a92b335926fb5ea7f2dc7cd
Code Stuff <beau@codestuff.us>
2019-04-09 14:56:54 -0700

    relational data + new category screen:  - data model updated to associate category for each task item  - todo - make methods for new view controller for category screen

---
+++ Todoey/Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by shiMac on 4/9/19.
+//  Copyright © 2019 code stuff. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+    // MARK: TableView data source methods
+    
+    
+    
+    
+    
+    //MARK: Datam manipulation methods
+    
+    
+    
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    
+
+    //MARK: TableView delegation methods
+    
+    
+    
+    
+}
